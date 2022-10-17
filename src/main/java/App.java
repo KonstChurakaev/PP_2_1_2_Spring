a5ac6f9d8a7182dba67f057cdde4c370a998e424
KonstChurakaev <111491525+KonstChurakaev@users.noreply.github.com>
2022-10-17 14:29:53 +0500

    versin 3( исправлен App)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,19 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
 
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean1 =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean1.getMessage());
-        HelloWorld bean2 =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
 //        System.out.println("check helloworld");
         System.out.println(bean1==bean2);
 
-        Cat cat1= (Cat) applicationContext.getBean("cat");
-        Cat cat2= (Cat) applicationContext.getBean("cat");
+        Cat cat1= applicationContext.getBean("cat", Cat.class);
+        Cat cat2= applicationContext.getBean("cat", Cat.class);
 
 //        System.out.println("check cat");
         System.out.println(cat1==cat2);
